10993a74c06887adf74ebdf8033ffa9b3d1b9d93
Fariha Rana <149359705+Fariha-Rana@users.noreply.github.com>
2023-11-16 21:11:40 +0530

    Update ChatRoom.js

--- src/component/ChatRoom.js
+++ src/component/ChatRoom.js
@@ -64,7 +64,7 @@ export default function ChatComponent() {
           overflowY="auto"
           margin={{ base: "10px", md: "0" }}
         >
-          {messages.reverse().map((message) => (
+          {messages.map((message) => (
             <Box
               key={message.$id}
               p={2}
